94d3f216a08951de2f49c7b96c80cce13adb04d4
Cleiton Luiz <cluiz@mcontigo.com>
2017-09-01 13:47:14 -0300

    Finalização do parse dos path usando Regex

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import Router from './core/scripts/router';
 const router = new Router();
 // router.register('/', 'login.html');
 // router.register('/teste', 'teste.html');
-router.register(/\/teste\/(\w*)/, 'teste_parametro.html');
+router.register(/\/teste\/(\w*)$/, 'teste_parametro.html');
 
 const app = new App(router);
 
